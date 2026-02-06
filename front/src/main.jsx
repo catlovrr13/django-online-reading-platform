ce4b38576da0f0991126683e75e62cdfcefaa881
algae-byte <diamondster5girl@gmail.com>
2026-02-06 13:30:17 +0800

    new react project (fresh)

--- front/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router";
-import { router } from "./routes/router.jsx";
-
-import "./index.css";
-
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
