623842b7ede0ebdda4d2a3276da8e5860235c913
algae-byte <diamondster5girl@gmail.com>
2026-02-12 15:04:46 +0800

    d

--- front/src/pages/home/Dashboard.tsx
+++ front/src/pages/home/Dashboard.tsx
@@ -10,7 +10,7 @@ function Dashboard() {
 
     return (
         <>
-            <div className="flex flex-col items-center justify-center h-screen">
+            <div className="flex flex-col items-center justify-center h-s">
             <h1>dashboard</h1>
             </div>
         </>
