caac33c47715d37a1e2aaa980c7f1b7bb4e96557
algae-byte <diamondster5girl@gmail.com>
2026-02-20 16:20:57 +0800

    small changes

--- front/src/pages/main/Dashboard.tsx
+++ front/src/pages/main/Dashboard.tsx
@@ -20,6 +20,7 @@ import mLight from "@/assets/media/hero-m-light.svg"
 
 // function
 import { useTheme } from "@/components/theme-provider";
+import Autoplay from "embla-carousel-autoplay"
 
 export const iframeHeight = "800px";
 
@@ -29,33 +30,43 @@ export default function Page() {
     theme === "dark" ? `bg-stone-900 opacity-40` : `bg-stone-200 opacity-40`;
   const cardAd =
     theme === "dark" ? `bg-stone-100 opacity-10` : `bg-stone-900 opacity-20`;
-    const heroImage = theme === 'dark' ? dDark : dLight;
+  const heroImage = theme === 'dark'
+    ? <img src={mDark} alt="hero mobile dark" className="md:hidden rotate-90 w-full h-full object-cover rounded-r-[100px]" />
+    : <img src={mLight} alt="hero mobile light" className="md:hidden rotate-90 w-full h-full object-cover rounded-r-[100px]" />;
+  const heroImageDesktop = theme === 'dark' ? dDark : dLight;
 
   return (
     <>
       <div className="flex flex-col gap-0 justify-center">
         {/* hero section */}
-          <div className="relative flex overflow-hidden rounded-b-[100px] mb-10 drop-shadow-lg dark:drop-shadow-stone-900 dark:drop-shadow-xl">
-            <div className="absolute z-10 flex flex-col md:flex-row p-10">
-              <div>
+          <div className="relative flex overflow-hidden rounded-b-[100px] mb-10 drop-shadow-lg dark:drop-shadow-stone-900 dark:drop-shadow-xl min-h-[400px] min-w-[400px]">
+            {/* content */}
+            <div className="absolute z-10 flex flex-col md:flex-row p-10 gap-5 justify-evenly w-full h-full">
+              <div className="flex-1 flex-col flex gap-5">
                 <Header1 text={"theonyxpub."} cl={'text-3xl md:text-5xl'}/>
-                <Header3 text={"hevccjsgdv"} />
+                <Header2 text={"this is an online reading platform haha"} cl={'font-light'}/>
               </div>
-              <div>
-
+              <div className="flex-1 content-end">
+                <Header3 text={"heh"} />
               </div>
             </div>
-            <img
-              src={heroImage}
-              alt="hero"
-              className="w-full h-[90%] object-cover rounded-b-[100px] scale-[1.01]"
-            />
+            {/* background */}
+            <div className="relative w-full h-full md:h-full md:w-full aspect-square md:aspect-auto object-cover rounded-b-[100px]">
+              <div className="md:hidden w-full h-full flex items-center justify-center rounded-b-[100px]">
+                {heroImage}
+              </div>
+              <img
+                src={heroImageDesktop}
+                alt="hero"
+                className="hidden md:block w-full h-full object-cover rounded-b-[100px] min-h-[300px] min-w-[300px]"
+              />
+            </div>
           </div>
 
 
         {/* genre */}
         <div
-          className={`overflow-hidden w-full max-w-2xl flex flex-col gap-5 p-5 justify-center items-center`}
+          className={`overflow-hidden w-full  flex flex-col gap-5 p-5 justify-center items-center`}
         >
           <ScrollFloat
             animationDuration={3}
@@ -63,6 +74,7 @@ export default function Page() {
             scrollStart="center bottom+=50%"
             scrollEnd="bottom bottom-=40%"
             stagger={0.04}
+            
           >
             Genres
           </ScrollFloat>
@@ -70,7 +82,12 @@ export default function Page() {
             opts={{
               align: "start",
             }}
-            className="w-full max-w-[12rem] sm:max-w-xs md:max-w-sm"
+            plugins={[
+              Autoplay({
+                delay: 2000,
+              }),
+            ]}
+            className="w-full max-w-[20rem] sm:max-w-xs md:max-w-sm "
           >
             <CarouselContent>
               {Array.from({ length: 5 }).map((_, index) => (
